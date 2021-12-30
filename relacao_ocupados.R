860312631e5d500662f371e3ea3bf3da67f6044b
Ledson L. G. da Rosa <77404105+LLeed@users.noreply.github.com>
2021-12-30 16:10:29 -0300

    Update relacao_ocupados.R

--- relacao_ocupados.R
+++ relacao_ocupados.R
@@ -1,3 +1,10 @@
+# Definindo limite de memoria para compilacao do programa
+aviso <- getOption("warn")
+options(warn=-1)
+memory.limit(size=20000)
+options(warn=aviso)
+rm(aviso)
+
 library(PNADcIBGE)
 library(tidyverse)
 library(survey)
@@ -6,13 +13,15 @@ library(data.table)
 var_select <- c("VD4002", "V2009", "VD4019", "VD3004", "VD4010", 
                 "VD4008", "VD4009", "VD4011" ,"VD4012")
 
-anos <-as.list(rep(2012:2021, times=4))
-trimestre <-as.list(rep(1:4, times=10))
+anos <-as.list(rep(2012:2015, each=4))
+trimestre <-as.list(rep(1:4, times=4))
 
 for (i in seq_along(anos)) {
   
   a <- get_pnadc(year = anos[[i]], quarter = trimestre[[i]], vars = var_select)
+  gc()
   assign(paste0("pnad",anos[[i]], trimestre[[i]], sep = "_" ), a )
-  
+  gc()
   
 }
+
